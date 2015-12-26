1f686d63e292fb215aceb3e99f52433eb83674e8
Shuxu <shuxu.tian@gallaudet.edu>
2015-12-25 17:30:21 -0800

    added new tool: Grunt!!!!
    - organized app file
    - put all front page files into “front_pages”
    - next step: gulp!
    - separate front pages and app pages

---
+++ app/scripts/app.js
@@ -0,0 +1,69 @@
+'use strict';
+angular.module('categoryApp', [])
+
+        .controller('categoryController', function(){
+            
+            this.tab = 1;
+            this.filtText = '';
+            
+            var videos=[
+                         {
+                            name: 'The Baobab',
+                            image: 'images/baobab.png',
+                            genre: 'Fiction',
+                            language: 'ASL',
+                            price: '4.99',
+                            description: 'The Baobab is an original story about a curious little girl who embarks on an adventure. Complete with enthralling illustrations and talented American Sign Language (ASL) storytelling, this bilingual interactive storybook app features a rich American Sign Language glossary with 170 English to ASL words.'
+
+                        },
+                        {
+                           name: 'The Boy Who Cried Wolf',
+                           image: 'images/boywolf.png',
+                           genre: 'fiction',
+                           language: 'ASL',
+                           price: '4.99',
+                           description: 'The classic Aesop&#146;s fable about the boy who cried wolf is brought to life in a wholly new medium with vivid American Sign Language storytelling, adding cinematic elements to a timeless tale. Accompanied by detailed paintings that evoke times of yore, this storybook app for the iPad comes with over 140 vocabulary words, signed and fingerspelled. App design is based on proven research on bilingualism and visual learning from Visual Language and Visual Learning.'
+                        },
+                        {
+                            name: 'The Solar System',
+                            image: 'images/solar_system.png',
+                            genre: 'non_F',
+                            language: 'ASL',
+                            price: '2.99',
+                            description: 'The Solar System is an educational narrative in both ASL and English about our eight planets, moons, and the sun. The illustrations and the video narrative provides enhanced visual aids to create an interactive, self-directed reading and learning experience for young deaf children. The Solar System is full of scientific facts along with a fantastic collection of vocabulary words and definitions in ASL.'
+                        },
+                        {
+                            name: 'The Blue Lobster',
+                            image: 'images/blue_lobster.png',
+                            genre: 'emerging_readers',
+                            language: 'ASL',
+                            price: '2.99',
+                            description: 'From the same team who made the award-winning ASL/English bilingual storybook app, The Baobab, we now bring you The Blue Lobster! The adventures of the curious little girl continues! In this storybook designed for younger and emerging readers, ages 3 & up, the curious little girl goes off in search for a rare blue lobster!'
+                        }  
+                        ];
+            
+            this.videos = videos;
+            
+            this.select = function(setTab)            {
+                this.tab = setTab;
+                
+                if (setTab === 2){
+                    this.filtText = "fiction";
+                }
+                else if (setTab === 3){
+                    this.filtText = "non_F";
+                }
+                else if (setTab === 4){
+                    this.filtText = "emerging_readers";
+                }
+                else{
+                    this.filtText = '';
+                }
+                
+            };
+            
+            this.isSelected = function(checkTab){
+                return (this.tab ===checkTab);
+            };
+
+        });
